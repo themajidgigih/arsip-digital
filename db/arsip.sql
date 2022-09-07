4843ec08722956f87b934f6f615b8b3fe7991613
Majid Gigih Setiawan <themajidgigihsetiawan@gmail.com>
2022-09-07 10:34:53 +0700

    menambah database

---
+++ db/arsip.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 30, 2022 at 06:08 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `arsip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_arsip`
+--
+
+CREATE TABLE `tbl_arsip` (
+  `id_arsip` int(11) NOT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `no_arsip` varchar(15) DEFAULT NULL,
+  `nama_arsip` varchar(255) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tgl_upload` date DEFAULT NULL,
+  `tgl_update` date DEFAULT NULL,
+  `file_arsip` varchar(255) DEFAULT NULL,
+  `ukuran_file` int(11) DEFAULT NULL,
+  `id_dep` int(11) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_arsip`
+--
+
+INSERT INTO `tbl_arsip` (`id_arsip`, `id_kategori`, `no_arsip`, `nama_arsip`, `deskripsi`, `tgl_upload`, `tgl_update`, `file_arsip`, `ukuran_file`, `id_dep`, `id_user`) VALUES
+(1, 5, '20211031-001', 'Surat Tugas SIRCLE 2019/2020', 'Surat Tugas Organisasi Internal Kampus', '2021-10-31', '2021-11-01', '1635756574_312f381249fddf954549.pdf', 436910, 1, 1),
+(8, 14, '22070631-X3iZ', 'Sertifikat TOEFL Majid Gigih Setiawan', 'Sertifikat TOEFL                                                ', '2022-07-06', '2022-08-08', '1657104709_921a658c22c258443a4a.pdf', 231518, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_dep`
+--
+
+CREATE TABLE `tbl_dep` (
+  `id_dep` int(11) NOT NULL,
+  `nama_dep` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_dep`
+--
+
+INSERT INTO `tbl_dep` (`id_dep`, `nama_dep`) VALUES
+(1, 'Keuangan'),
+(2, 'Hubungan Masyarakat'),
+(6, 'Administrasi'),
+(7, 'PKK');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'Surat Masuk'),
+(2, 'Surat Keluar'),
+(3, 'Berkas Kerja'),
+(4, 'Surat Keputusan'),
+(5, 'Surat Tugas'),
+(12, 'Data Warga'),
+(13, 'Arsip Internal'),
+(14, 'Arsip Eksternal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL,
+  `foto` varchar(250) DEFAULT NULL,
+  `id_dep` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `email`, `password`, `level`, `foto`, `id_dep`) VALUES
+(1, 'Majid Gigih Setiawan', 'themajidgigihsetiawan@gmail.com', '12345', 1, '1.png', 1),
+(3, 'Naila Meilina Putri', 'naila@gmail.com', 'naila123', 2, '1635697673_e01d7fdbac74691541e5.png', 6),
+(5, 'Yusuf Awaludin', 'iosef@protonmail.com', 'iosef123', 3, '1656906524_025b89a6d64f6722013d.png', 2),
+(7, 'qwerty', 'qwerty@gmail.com', 'qwerty', 2, '1660516832_b8f6bdb3a3b66fe0f745.png', 7);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  ADD PRIMARY KEY (`id_arsip`);
+
+--
+-- Indexes for table `tbl_dep`
+--
+ALTER TABLE `tbl_dep`
+  ADD PRIMARY KEY (`id_dep`);
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_arsip`
+--
+ALTER TABLE `tbl_arsip`
+  MODIFY `id_arsip` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_dep`
+--
+ALTER TABLE `tbl_dep`
+  MODIFY `id_dep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
